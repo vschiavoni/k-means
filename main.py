e24233bb0464db1e6b593a519cf9e06ad0a014a0
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-07-18 17:38:36 +0200

    read input file and K value as command-line arguments

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import random as rand
 from clustering import clustering
 from point import Point
 import csv
+import sys
 
 geo_locs = []
 #loc_ = Point(0.0, 0.0)  #tuples for location
 #geo_locs.append(loc_)
 #read the fountains location from the csv input file and store each fountain location as a Point(latit,longit) object
-f = open('/home/kazem/Downloads/Hackathon/drinkingFountains.csv', 'r')
+if len(sys.argv)>2:
+    print "Input CSV file:" + sys.argv[2]
+    f=     open(sys.argv[2], 'r')
+else:
+    print "Input CSV file: it-2004.sites.gpscoords.csv"   
+    f = open('it-2004.sites.gpscoords.csv', 'r')
 reader = csv.reader(f, delimiter=",")
 for line in reader:
     loc_ = Point(float(line[0]), float(line[1]))  #tuples for location
@@ -16,7 +22,9 @@ for line in reader:
 #for p in geo_locs:
 #    print "%f %f" % (p.latit, p.longit)
 #let's run k_means clustering. the second parameter is the no of clusters
-cluster = clustering(geo_locs, 8 )
+k_value = sys.argv[1]
+print "K_Value: " + k_value
+cluster = clustering(geo_locs, int(k_value))
 flag = cluster.k_means(False)
 if flag == -1:
     print "Error in arguments!"
